607c8bfdcd79f0d1a6ba542f51b7be8470caaaeb
Aaron McConnell <amcconnell@duosecurity.com>
2021-08-13 14:50:04 -0400

    Set up skeleton of project

---
+++ DuoUniversal.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace DuoUniversal.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
