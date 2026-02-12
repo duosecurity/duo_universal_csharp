90534a7590d134d19de1679b6a5acee3bbf2f2c3
tcotidiane33 <tcotidiane33@gmail.com>
2026-02-12 10:01:42 +0000

    feat: Add published build artifacts for the DuoUniversal example application and update its callback page.

--- DuoUniversal.Example/Pages/Callback.cshtml.cs
+++ DuoUniversal.Example/Pages/Callback.cshtml.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace DuoUniversal.Example.Pages
 {
