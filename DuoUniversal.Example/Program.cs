143d88d35cf1f0aa2f18ee85965e2a186c0bae5e
tcotidiane33 <tcotidiane33@gmail.com>
2026-02-12 10:55:53 +0000

    Move `StringExtensions` class inside `Program` class.

--- DuoUniversal.Example/Program.cs
+++ DuoUniversal.Example/Program.cs
@@ -52,16 +52,15 @@ namespace DuoUniversal.Example
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-}
-
-// Add this helper for Linq Select since we are in a simple file
-internal static class StringExtensions
-{
-    public static string ToTitleCase(this string str)
-    {
-        if (string.IsNullOrEmpty(str)) return str;
-        return char.ToUpper(str[0]) + str.Substring(1).ToLower();
     }
-}
+
+    // Add this helper for Linq Select since we are in a simple file
+    internal static class StringExtensions
+    {
+        public static string ToTitleCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
     }
 }
